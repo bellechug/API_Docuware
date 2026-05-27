9891a65785b57d89c5f046c2782b2d201309592a
hugo <hugo.bellec@isen-ouest.yncrea.fr>
2026-05-27 11:03:18 +0200

    add api key security

--- validationsBeforeStoring.js
+++ validationsBeforeStoring.js
@@ -16,9 +16,12 @@ exports.checkValues = function (DWInputValues) {
     }
 
     const montantHT = parseFloat(String(montantHTField.Item).replace(',', '.'));
+    
     if (montantHT <= 0){
-      message: 'Le Montant HT est soit = ou inférieur à 0'
-    }
+      return {
+        success: false,
+        message: 'Le Montant HT est soit = ou inférieur à 0'
+    }}
 
     const montantTVA = montantTvaField 
       ? parseFloat(String(montantTvaField.Item).replace(',', '.'))
